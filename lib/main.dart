05b512f74ab71ce1d9d02031c00ea8ff6c7b1ef3
Naki-ym <0141tsubuann@gmail.com>
2024-08-10 19:01:54 +0900

    feat: finish step3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_pokemon/poke_list_item.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,53 +15,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const TopPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
+class TopPage extends StatelessWidget {
+  const TopPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.network(
-                        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-                        height: 100,
-                        width: 100,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  child: const Text(
-                    'No. 25',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                )
-              ],
-            ),
-            const Text(
-              'pikachu',
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-            const Chip(
-              label: Text('electric'),
-              backgroundColor: Colors.yellow,
-            )
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: 10000,
+        itemBuilder: (context, index) => PokeListItem(index: index,)
       ),
     );
   }
