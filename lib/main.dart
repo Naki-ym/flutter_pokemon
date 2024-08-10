a389d2291282988b3e5e2e8379d1ab569405c55f
Naki-ym <0141tsubuann@gmail.com>
2024-08-10 22:53:59 +0900

    feat: finish step4

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: const TopPage(),
     );
   }
