1038463860cb59cf1c8704c7df2018f2fa64f347
Naki-ym <0141tsubuann@gmail.com>
2024-08-11 21:30:36 +0900

    feat: finish step5

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_pokemon/poke_list_item.dart';
 
 void main() {
@@ -18,15 +19,68 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TopPage extends StatelessWidget {
+class TopPage extends StatefulWidget {
   const TopPage({super.key});
+
+  @override
+  State<TopPage> createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  int currentbnb = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10000,
-        itemBuilder: (context, index) => PokeListItem(index: index,)
+      body: SafeArea(
+        child: currentbnb == 0 ? const PokeList() : const Settings(),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => {
+          setState(
+            () => currentbnb = index,
+          ),
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PokeList extends StatelessWidget {
+  const PokeList({super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+      itemCount: 10000,
+      itemBuilder: (context, index) => PokeListItem(index: index),
+    );
+  }
+}
+
+class Settings extends StatelessWidget {
+  const Settings({super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        ListTile(
+          leading: Icon(Icons.lightbulb),
+          title: Text('Dark/Light Mode'),
+        ),
+      ],
     );
   }
 }
