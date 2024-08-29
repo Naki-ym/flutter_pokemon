b64405c0908022ffb4b26d28e30dbb961e6eb539
Naki-ym <0141tsubuann@gmail.com>
2024-08-29 20:19:50 +0900

    feat: トップ画面で戻るとアプリが落ちる問題の対策

--- lib/main.dart
+++ lib/main.dart
@@ -63,26 +63,30 @@ class _TopPageState extends State<TopPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: currentbnb == 0 ? const PokeList() : const Settings(),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) => {
-          setState(
-            () => currentbnb = index,
-          ),
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'settings',
-          ),
-        ],
+    return PopScope(
+      canPop: false,
+      // TODO: onPopInvokedで確認ダイアログを出すようにする
+      child: Scaffold(
+        body: SafeArea(
+          child: currentbnb == 0 ? const PokeList() : const Settings(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) => {
+            setState(
+              () => currentbnb = index,
+            ),
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'settings',
+            ),
+          ],
+        ),
       ),
     );
   }
