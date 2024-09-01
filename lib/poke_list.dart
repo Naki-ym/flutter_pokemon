5c1abd2ad44e27f8b4ec2e1e3795c8a6f6e767dd
Naki-ym <0141tsubuann@gmail.com>
2024-09-01 21:32:39 +0900

    feat: finish step9-1

--- lib/poke_list.dart
+++ lib/poke_list.dart
@@ -4,6 +4,7 @@ import 'package:flutter_pokemon/models/pokemon.dart';
 import 'package:flutter_pokemon/models/favorite.dart';
 import 'package:flutter_pokemon/const/pokeapi.dart';
 import 'package:flutter_pokemon/poke_list_item.dart';
+import 'package:flutter_pokemon/poke_grid_item.dart';
 
 class PokeList extends StatefulWidget {
   const PokeList({super.key,});
@@ -14,6 +15,7 @@ class PokeList extends StatefulWidget {
 class _PokeListState extends State<PokeList> {
   static const int pageSize = 30;
   bool isFavoriteMode = false;
+  bool isGridMode = true;
   int _currentPage = 1;
 
   bool isLastPage(int favscount, int page) {
@@ -44,8 +46,12 @@ class _PokeListState extends State<PokeList> {
     return ret;
   }
 
-  void changeMode(bool currentMode) {
+  void changeFavMode(bool currentMode) {
     setState(() => isFavoriteMode = !currentMode);
+  }
+
+  void changeGridMode(bool currentMode) {
+    setState(() => isGridMode = !currentMode);
   }
 
   @override
@@ -60,7 +66,7 @@ class _PokeListState extends State<PokeList> {
               padding: const EdgeInsets.all(0),
               icon: const Icon(Icons.auto_awesome_outlined),
               onPressed: () async {
-                var ret = await showModalBottomSheet<bool>(
+                await showModalBottomSheet<bool>(
                   context: context,
                   shape: const RoundedRectangleBorder(
                     borderRadius: BorderRadius.only(
@@ -71,12 +77,12 @@ class _PokeListState extends State<PokeList> {
                   builder: (BuildContext context){
                     return ViewModeBottomSheet(
                       favMode: isFavoriteMode,
+                      changeFavMode: changeFavMode,
+                      gridMode: isGridMode,
+                      changeGridMode: changeGridMode,
                     );
                   },
                 );
-                if(ret != null && ret) {
-                  changeMode(isFavoriteMode);
-                }
               },
             ),
           ),
@@ -86,24 +92,57 @@ class _PokeListState extends State<PokeList> {
                 if(itemCount(favs.favs.length, _currentPage) == 0) {
                   return const Text('no data');
                 } else {
-                  return ListView.builder(
-                    padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-                    itemCount: itemCount(favs.favs.length, _currentPage) + (isLastPage(favs.favs.length, _currentPage) ? 0 : 1),
-                    itemBuilder: (context, index) {
-                      if (index == itemCount(favs.favs.length, _currentPage)) {
-                        return OutlinedButton(
-                          child: const Text('more'),
-                          onPressed: () => {
-                            setState(() => _currentPage++),
-                          },
-                        );
-                      } else {
-                        return PokeListItem(
-                          poke: pokes.byId(itemId(favs.favs, index)),
-                        );
+                  if(isGridMode) {
+                    return GridView.builder(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3
+                      ),
+                      itemCount: itemCount(favs.favs.length, _currentPage) + (isLastPage(favs.favs.length, _currentPage) ? 0 : 1),
+                      itemBuilder: (context, index) {
+                        if (index == itemCount(favs.favs.length, _currentPage)) {
+                          return Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              onPressed: isLastPage(favs.favs.length, _currentPage)
+                                ? null
+                                : () => {
+                                  setState(() => _currentPage++),
+                                },
+                              child: const Text('more'),
+                            ),
+                          );
+                        } else {
+                          return PokeGridItem(
+                            poke: pokes.byId(itemId(favs.favs, index)),
+                          );
+                        }
                       }
-                    }
-                  );
+                    );
+                  } else {
+                    return ListView.builder(
+                      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                      itemCount: itemCount(favs.favs.length, _currentPage) + (isLastPage(favs.favs.length, _currentPage) ? 0 : 1),
+                      itemBuilder: (context, index) {
+                        if (index == itemCount(favs.favs.length, _currentPage)) {
+                          return OutlinedButton(
+                            child: const Text('more'),
+                            onPressed: () => {
+                              setState(() => _currentPage++),
+                            },
+                          );
+                        } else {
+                          return PokeListItem(
+                            poke: pokes.byId(itemId(favs.favs, index)),
+                          );
+                        }
+                      }
+                    );
+                  }
                 }
               }
             ),
@@ -118,18 +157,20 @@ class ViewModeBottomSheet extends StatelessWidget {
   const ViewModeBottomSheet({
     super.key,
     required this.favMode,
+    required this.changeFavMode,
+    required this.gridMode,
+    required this.changeGridMode,
   });
   final bool favMode;
+  final Function(bool) changeFavMode;
+  final bool gridMode;
+  final Function(bool) changeGridMode;
 
-  String mainText(bool fav) {
-    if (fav) {
-      return 'お気に入りのポケモンが表示されています';
-    } else {
-      return 'すべてのポケモンが表示されています';
-    }
+  String mainText() {
+    return '表示設定';
   }
 
-  String menuTitle(bool fav) {
+  String menuFavTitle(bool fav) {
     if (fav) {
       return '「すべて」表示に切り替え';
     } else {
@@ -137,11 +178,27 @@ class ViewModeBottomSheet extends StatelessWidget {
     }
   }
 
-  String menuSubtitle(bool fav) {
+  String menuFavSubtitle(bool fav) {
     if (fav) {
       return 'すべてのポケモンが表示されます';
     } else {
       return 'お気に入りに登録したポケモンのみが表示されます';
+    }
+  }
+
+  String menuGridTitle(bool grid) {
+    if (grid) {
+      return 'リスト表示に切り替え';
+    } else {
+      return 'グリッド表示に切り替え';
+    }
+  }
+
+  String menuGridSubtitle(bool grid) {
+    if (grid) {
+      return 'ポケモンをグリッド表示します';
+    } else {
+      return 'ポケモンをリスト表示します';
     }
   }
 
@@ -164,7 +221,7 @@ class ViewModeBottomSheet extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
               child: Text(
-                mainText(favMode),
+                mainText(),
                 style: const TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
@@ -174,12 +231,26 @@ class ViewModeBottomSheet extends StatelessWidget {
             ListTile(
               leading: const Icon(Icons.swap_horiz),
               title: Text(
-                menuTitle(favMode),
+                menuFavTitle(favMode),
               ),
               subtitle: Text(
-                menuSubtitle(favMode),
+                menuFavSubtitle(favMode),
               ),
               onTap: () {
+                changeFavMode(favMode);
+                Navigator.pop(context, true);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.grid_3x3),
+              title: Text(
+                menuGridTitle(favMode),
+              ),
+              subtitle: Text(
+                menuGridSubtitle(favMode),
+              ),
+              onTap: () {
+                changeGridMode(gridMode);
                 Navigator.pop(context, true);
               },
             ),
